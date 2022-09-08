ab2989e2b2cc5eafd9b910bde08237e4b0acf8dd
golti saikiran <golti.saikiran@infosys.com>
2022-09-08 17:06:22 +0530

    changes

--- test-project/src/components/InputScreen/BEtrend/BEtrend.js
+++ test-project/src/components/InputScreen/BEtrend/BEtrend.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import axios  from 'axios';
 import {useState, useEffect} from 'react'
-// import { DataGrid, GridColDef, GridToolbar, GridValueGetterParams } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridToolbar, GridValueGetterParams } from '@mui/x-data-grid';
 import DataTable from 'react-data-table-component'
 
 
@@ -12,26 +12,40 @@ import DataTable from 'react-data-table-component'
 function BEtrend() {
   const [data,setData]=useState([]);
   const columns =[
-    // {headerName:"id",field:"id"},
-    {name:"vertical",
-  selector:row=>row.vertical},
+    {headerName:"id",field:"id"},
+    // {name:"vertical",
+  // selector:row=>row.vertical},
     {name:"apr",selector:row=>row.apr},
-    {name:"may",selector:row=>row.may},
-    {name:"jun",selector:row=>row.jun},
-    // {headerName:"Q1Sum",valueGetter: getQ1Sum,field:"Q1sum"},
-    // {headerName:"jul",field:"jul",editable:"true"},
-    // {headerName:"aug",field:"aug",editable:"true"},
-    // {headerName:"sep",field:"sep",editable:"true"},
-    // {headerName:"Q2Sum",valueGetter: getQ2Sum,field:"Q2sum"},
-    // {headerName:"oct",field:"oct",editable:"true"},
-    // {headerName:"nov",field:"nov",editable:"true"},
-    // {headerName:"dec",field:"dec",editable:"true"},
-    // {headerName:"Q3Sum",valueGetter: getQ3Sum,field:"Q3sum"},
-    // {headerName:"Jan",field:"jan",editable:"true"},
-    // {headerName:"Feb",field:"feb",editable:"true"},
-    // {headerName:"Mar",field:"mar",editable:"true"},
-    // {headerName:"Q4Sum",valueGetter: getQ4Sum,field:"Q4sum"},
-  
+    // {name:"may",selector:row=>row.may},
+    // {name:"jun",selector:row=>row.jun},
+    //  {name:"Q1Sum",selector:row=>getQ1Sum},
+    // {name:"jul",selector:row=>row.jul},
+    // {name:"aug",selector:row=>row.aug},
+    // {name:"sep",selector:row=>row.sep},
+    // {name:"Q2Sum",valueGetter: getQ2Sum,field:"Q2sum"},
+    // {name:"oct",selector:row=>row.oct},
+    // {name:"nov",selector:row=>row.nov},
+    // {name:"dec",selector:row=>row.dec},
+    // {name:"Q3Sum",valueGetter: getQ3Sum,field:"Q3sum"},
+    // {name:"Jan",selector:row=>row.jan},
+    // {name:"Feb",selector:row=>row.feb},
+    // {name:"Mar",selector:row=>row.mar},
+    // {name:"Q4Sum",valueGetter: getQ4Sum,field:"Q4sum"},
+    // {name:"Action",cell:(row)=><button className="btn btn-primary" onClick={()=>alert(row.id)}>Delete</button>},
+    {headerName:"Q1Sum",valueGetter: getQ1Sum,field:"Q1sum"},
+    {headerName:"jul",field:"jul",editable:"true"},
+    {headerName:"aug",field:"aug",editable:"true"},
+    {headerName:"sep",field:"sep",editable:"true"},
+    {headerName:"Q2Sum",valueGetter: getQ2Sum,field:"Q2sum"},
+    {headerName:"oct",field:"oct",editable:"true"},
+    {headerName:"nov",field:"nov",editable:"true"},
+    {headerName:"dec",field:"dec",editable:"true"},
+    {headerName:"Q3Sum",valueGetter: getQ3Sum,field:"Q3sum"},
+    {headerName:"Jan",field:"jan",editable:"true"},
+    {headerName:"Feb",field:"feb",editable:"true"},
+    {headerName:"Mar",field:"mar",editable:"true"},
+    {headerName:"Q4Sum",valueGetter: getQ4Sum,field:"Q4sum"},
+    {headerName:"Action",field:"Q4su"},
   ];
   const fetch=()=>{
     axios
@@ -48,18 +62,18 @@ function BEtrend() {
    useEffect(()=>{
   fetch();
   },[])
-  // function getQ1Sum(params) {
-  //   return Number(params.row.apr)+Number(params.row.may)+Number(params.row.jun) ;
-  // }
-  // function getQ2Sum(params) {
-  //   return Number(params.row.jul)+Number(params.row.aug)+Number(params.row.sep) ;
-  // } 
-  //  function getQ3Sum(params) {
-  //   return Number(params.row.oct)+Number(params.row.nov)+Number(params.row.dec) ;
-  // }  
-  // function getQ4Sum(params) {
-  //   return Number(params.row.jan)+Number(params.row.feb)+Number(params.row.mar) ;
-  // }
+  function getQ1Sum(params) {
+    return Number(params.row.apr)+Number(params.row.may)+Number(params.row.jun) ;
+  }
+  function getQ2Sum(params) {
+    return Number(params.row.jul)+Number(params.row.aug)+Number(params.row.sep) ;
+  } 
+   function getQ3Sum(params) {
+    return Number(params.row.oct)+Number(params.row.nov)+Number(params.row.dec) ;
+  }  
+  function getQ4Sum(params) {
+    return Number(params.row.jan)+Number(params.row.feb)+Number(params.row.mar) ;
+  }
 
 const rows =data.map((row)=>({
   id:row.id,
@@ -71,16 +85,27 @@ const rows =data.map((row)=>({
   return (
     <div>
        <Box sx={{ height: 500, width: '100%' }}>
-<DataTable columns={columns} data={data}
-      //     components={{Toolbar:GridToolbar}}
-      //     onCellEditCommit={(params)=>console.log(data)}
-      //     autoHeight
-      //     pageSize={10}    
-      //     rowsPerPageOptions={[10]}
-      //     checkboxSelection
-      // disableSelectionOnClick
-      // experimentalFeatures={{ newEditingApi: true }}
+<DataGrid  columns={columns} rows={data} 
+           
+          components={{Toolbar:GridToolbar}}
+          onCellEditCommit={(params)=>console.log(data)}
+          autoHeight
+          pageSize={10}    
+          rowsPerPageOptions={[10]}
+          checkboxSelection
+      disableSelectionOnClick
+      experimentalFeatures={{ newEditingApi: true }}
   />
+  <DataTable  columns={columns} rows={data} 
+           fixedHeader fixedHeaderScrollHeight="600px"
+              selectableRows
+             selectableRowsHighlight
+             highlightOnHover
+              actions={
+               <button className="btn btn-info">Export</button>
+              }
+            />
+
 </Box>
 
     </div>
